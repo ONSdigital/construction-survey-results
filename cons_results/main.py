617f24cc7f8126a5d7f628273f4e23ff3a9558b7
Anton Zogkolli <Anton.Zogkolli@ons.gov.uk>
2025-04-10 11:34:48 +0000

    Set placeholders in main script

--- cons_results/main.py
+++ cons_results/main.py
@@ -1,7 +1,42 @@
+import warnings
+
+# import load general config (JSON)
+# import staging
+# import imputation
+# import post-imputation
+# import estimation
+# import outlier detection
+
+# import staging validation checks
+# import imputation checks
+# import estimation checks
+
+
 def run_pipeline():
     """This is the main function that runs the pipeline"""
-    print("Running pipeline")
-    print("Pipeline finished")
+
+    warnings.warn(
+        "This is a placeholder for load general config (JSON),  not yet implemented"
+    )
+
+    warnings.warn("This is a placeholder for staging,  not yet implemented")
+    warnings.warn(
+        "This is a placeholder for staging validation checksng,  not yet implemented"
+    )
+
+    warnings.warn("This is a placeholder for imputation,  not yet implemented")
+    warnings.warn("This is a placeholder post-imputation,  not yet implemented")
+    warnings.warn(
+        "This is a placeholder for imputation validation checks,  not yet implemented"
+    )
+
+    warnings.warn("This is a placeholder for estimation,  not yet implemented")
+    warnings.warn(
+        "This is a placeholder for estimation validation checks,  not yet implemented"
+    )
+
+    warnings.warn("This is a placeholder for outlier detection,  not yet implemented")
+    warnings.warn("This is a placeholder for estimation,  not yet implemented")
 
 
 if __name__ == "__main__":
