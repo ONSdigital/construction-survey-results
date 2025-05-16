d32f247d4f56fb2f8ef00940554bac36df8669a9
ChrisSoderberg-ONS <141257618+ChrisSoderberg-ONS@users.noreply.github.com>
2025-05-16 13:40:42 +0100

    Add unit test for rescale_290_case

--- tests/imputation/test_imputation.py
+++ tests/imputation/test_imputation.py
@@ -1,1 +1,19 @@
-# PLACEHOLDER TO SETUP STRUCTURE
+import pandas as pd
+import pytest
+
+def test_rescale_290_case():
+    expected_output_df = pd.read_csv(
+        "tests/data/imputation/test_data_rescale_290_output.csv"
+    )
+
+    input_df = pd.read_csv(
+        "tests/data/imputation/test_data_rescale_290_output.csv",
+        dtype={"adjustedresponse": float},
+    )
+
+    output_df = rescale_290_case(
+        input_df, "period", "reference", "question_no", "adjustedresponse"
+    )
+
+    pd.testing.assert_frame_equal(output_df, expected_output_df)
+  
