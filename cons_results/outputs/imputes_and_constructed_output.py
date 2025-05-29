f0cb2168d797f97f60f340bee09520749a7407e5
Luke Davies <117165341+ldavies99@users.noreply.github.com>
2025-05-29 14:38:54 +0100

    Add imputes_and_constructed_output and unit test (#25)
    * Add imputes_and_constructed_output and unit test
    
    * Run hooks
    
    * Fix filename and add filepath to setup
    
    * Replace hard coded col name with config name
    
    * Another hard coded col
    
    * Rename test
    
    * Correct minor typo
    
    ---------
    
    Co-authored-by: NathanKelly-ONS <93586797+NathanKelly-ONS@users.noreply.github.com>

---
+++ cons_results/outputs/imputes_and_constructed_output.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+
+def get_imputes_and_constructed_output(
+    additional_outputs_df: pd.DataFrame, **config
+) -> pd.DataFrame:
+    """
+    Creates imputes and constructed output for current period on frozen runs only.
+
+    Parameters
+    ----------
+    additional_outputs_df : pd.DataFrame
+        The input DataFrame containing additional outputs.
+    config : dict
+        A dictionary containing configuration parameters. Must include:
+        - "state" : str
+            The state of the process. If not "frozen", the function returns None.
+        - "period" : str
+            The column name for the period.
+        - "current_period" : Any
+            The value of the current period.
+        - "reference" : str
+            The column name for the reference.
+        - "question_no" : str
+            The column name for the question number.
+        - "target" : str
+            The column name for the target data.
+        - "imputation_marker_col" : str
+            The column name for the imputation markers.
+
+    Returns
+    -------
+    pd.DataFrame
+        Imputes and construction output.
+        Returns None if not frozen run.
+    """
+    if config["state"] != "frozen":
+        return
+
+    additional_outputs_df = additional_outputs_df[
+        additional_outputs_df[config["period"]] == config["current_period"]
+    ]
+
+    imputes_and_constructed_output = additional_outputs_df[
+        [
+            config["reference"],
+            config["question_no"],
+            config["target"],
+            config["imputation_marker_col"],
+        ]
+    ]
+
+    imputes_and_constructed_output = imputes_and_constructed_output[
+        imputes_and_constructed_output[config["imputation_marker_col"]] != "r"
+    ]
+
+    imputes_and_constructed_output = imputes_and_constructed_output.rename(
+        columns={
+            config["target"]: "constructedresponse",
+            config["imputation_marker_col"]: "imputationmarker",
+        }
+    )
+
+    imputes_and_constructed_output.reset_index(drop=True, inplace=True)
+
+    return imputes_and_constructed_output
