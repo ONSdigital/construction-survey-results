8d514beffab8e97be1581c487a98fc884e3b7e4d
NathanKelly-ONS <93586797+NathanKelly-ONS@users.noreply.github.com>
2025-06-04 15:43:14 +0100

    Update derive_q290_outlier_weights documentation

--- cons_results/outlier_detection/derive_outlier_weights.py
+++ cons_results/outlier_detection/derive_outlier_weights.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def derive_q290_outlier_weights(
     df: pd.DataFrame,
-    config: dict,
+    all_questions: list,
     target: str,
     question_no: str,
     reference: str,
@@ -16,12 +16,20 @@ def derive_q290_outlier_weights(
 
     Parameters
     ----------
-    config : dict
-        Config dictionary containing all_questions list.
     df : pd.DataFrame
         Dataframe containing the necessary data for calculations.
         It should have at least the following columns:
         "reference", "period", "question_no", "o_weights", "adjustedresponse".
+    all_questions : list
+        A list containing all of the component questions.
+    target : str
+        The target variable column name.
+    question_no : str
+        The question_no variable column name.
+    reference : str
+        The reference variable column name.
+    period : str,
+        The period variable column name.
 
     Returns
     -------
@@ -30,7 +38,7 @@ def derive_q290_outlier_weights(
 
     Example
     -------
-    >>> config = {"all_questions": [201, 202, 211, 212, 221, 222]}
+    >>> all_questions: [201, 202, 211, 212, 221, 222]
     >>> df = {
     'reference': [1, 1, 1],
     'period': [202201, 202201, 202201],
@@ -38,16 +46,21 @@ def derive_q290_outlier_weights(
     'target':[4, 5, 9],
     'o_weights': [0.5, 0.2]
     }
-    >>> result = calculate_q290_o_weights(config, df)
+    >>> result = calculate_q290_o_weights(
+    df, 
+    all_questions, 
+    target, 
+    question_no, 
+    reference, 
+    period
+    )
     reference  period  questioncode  target  o_weights
     1    1  202201            201       4       0.5
     2    1  202201            202       5       0.2
     3    1  202201            290       9       0.33333
 
     """
-
-    all_questions = config["all_questions"]
-
+    
     # Using the formula to calculate q290 outlier weights
     components = df[df[question_no].isin(all_questions)]
     components.loc[:, "o_weight_times_value"] = (
