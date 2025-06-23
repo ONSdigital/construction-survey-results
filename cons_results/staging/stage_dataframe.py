d0b6e5007f5cf04a165fafad7c2485bb3c343d5d
daviel9 <Luke.Davies@ons.gov.uk>
2025-06-23 16:36:20 +0100

    Append back data to responses and contributors before create_missing_questions

--- cons_results/staging/stage_dataframe.py
+++ cons_results/staging/stage_dataframe.py
@@ -48,16 +48,27 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
         contributors, keep_columns=config["contributors_keep_cols"], **config
     )
 
+    contributors = append_back_data(contributors, config)
+
     responses = responses[config["responses_keep_cols"]]
     responses = enforce_datatypes(
         responses, keep_columns=config["responses_keep_cols"], **config
     )
 
-    finalsel = read_and_combine_colon_sep_files(config)
+    responses = append_back_data(responses, config)
+
+    back_data_config = config.copy()
+    back_data_config["revision_window"] = config["revision_window"] + 1
+    print("====================================================================")
+    print(back_data_config["revision_window"])
+
+    finalsel = read_and_combine_colon_sep_files(back_data_config)
 
     # keep columns is applied in data reading from source, enforcing dtypes
     # in all columns of finalsel
-    finalsel = enforce_datatypes(finalsel, keep_columns=list(finalsel), **config)
+    finalsel = enforce_datatypes(
+        finalsel, keep_columns=list(finalsel), **back_data_config
+    )
 
     # Filter contributors files here to temp fix this overlap
 
@@ -72,65 +83,72 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
     df = create_missing_questions(
         contributors=contributors,
         responses=responses,
-        all_questions=config["all_questions"],
-        reference=config["reference"],
-        period=config["period"],
-        question_col=config["question_no"],
+        all_questions=back_data_config["all_questions"],
+        reference=back_data_config["reference"],
+        period=back_data_config["period"],
+        question_col=back_data_config["question_no"],
     )
 
     df = pd.merge(left=df, right=contributors, on=[period, reference], how="left")
-
-    df = append_back_data(df, config)
 
     snapshot_name = os.path.basename(snapshot_file_path).split(".")[0]
 
     df = filter_out_questions(
         df=df,
-        column=config["question_no"],
-        questions_to_filter=config["filter_out_questions"],
-        save_full_path=config["output_path"]
+        column=back_data_config["question_no"],
+        questions_to_filter=back_data_config["filter_out_questions"],
+        save_full_path=back_data_config["output_path"]
         + snapshot_name
         + "_filter_out_questions.csv",
-        **config,
+        **back_data_config,
     )
 
     df = run_live_or_frozen(
         df,
-        config["target"],
-        status=config["status"],
-        state=config["state"],
+        back_data_config["target"],
+        status=back_data_config["status"],
+        state=back_data_config["state"],
         error_values=[201],
     )
 
-    df[config["auxiliary_converted"]] = df[config["auxiliary"]].copy()
-    df = convert_annual_thousands(df, config["auxiliary_converted"])
+    df[back_data_config["auxiliary_converted"]] = df[
+        back_data_config["auxiliary"]
+    ].copy()
+    df = convert_annual_thousands(df, back_data_config["auxiliary_converted"])
 
     df = derive_imputation_class(
-        df, config["bands"], config["cell_number"], config["imputation_class"]
+        df,
+        back_data_config["bands"],
+        back_data_config["cell_number"],
+        back_data_config["imputation_class"],
     )
 
-    if config["manual_constructions_path"]:
+    if back_data_config["manual_constructions_path"]:
         manual_constructions = read_csv_wrapper(
-            config["manual_constructions_path"], config["platform"], config["bucket"]
+            back_data_config["manual_constructions_path"],
+            back_data_config["platform"],
+            back_data_config["bucket"],
         )
     else:
         manual_constructions = None
 
-    if config["filter"]:
+    if back_data_config["filter"]:
         filter_df = read_csv_wrapper(
-            config["filter"], config["platform"], config["bucket"]
+            back_data_config["filter"],
+            back_data_config["platform"],
+            back_data_config["bucket"],
         )
-        filter_df = enforce_datatypes(filter_df, list(filter_df), **config)
+        filter_df = enforce_datatypes(filter_df, list(filter_df), **back_data_config)
 
     else:
         filter_df = None
 
     df = flag_290_case(
         df,
-        config["period"],
-        config["reference"],
-        config["question_no"],
-        config["target"],
+        back_data_config["period"],
+        back_data_config["reference"],
+        back_data_config["question_no"],
+        back_data_config["target"],
     )
 
     print("Staging Completed")
