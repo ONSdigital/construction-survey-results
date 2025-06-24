266cb29f59608ec0f387704ae16f1e98b67b0a4b
jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-06-24 11:07:10 +0100

    Created unit test for get_additional_outputs

--- cons_results/outputs/get_additional_outputs.py
+++ cons_results/outputs/get_additional_outputs.py
@@ -6,17 +6,47 @@ def get_additional_outputs(
     function_mapper: dict,
     additional_outputs_df: pd.DataFrame,
 ) -> dict:
-    additional_outputs = dict()
+    """
+    produces outputs by combining "additional_outputs" and "mandatory_outputs"
+    from the config file and running the functions from the function_mapper.
+    mandatory_outputs are always run and defined in the dev config file.
+    optional_outputs are defined in the user config file.
 
-    if not isinstance(config["additional_outputs"], list):
-        raise ValueError(
-            """
-            In config file additional_outputs must be a list, please use:\n
-            ["all"] to get all outputs\n
-            [] to get only mandatory outputs (defined in dev config) (\n
-            or a list with the outputs, e.g. ["output_1","output_2"]
-            """
-        )
+    Parameters
+    ----------
+    config : dict
+        main config file for pipeline
+    function_mapper : dict
+        dictionary with function names as keys and functions as values
+    additional_outputs_df : pd.DataFrame
+        dataframe used to produce outputs
+
+    Returns
+    -------
+    dict
+        dictionary with function names as keys and tuples of
+        (dataframe, name) as values, where dataframe is the output of the function
+
+    Raises
+    ------
+    TypeError
+        raises type error if "additional_outputs" or "mandatory_outputs" in config
+        are not lists or if they are not specified correctly
+    ValueError
+        raises value error if a function specified in "additional_outputs" or
+        "mandatory_outputs" is not registered in the function_mapper
+    """
+    additional_outputs = dict()
+    for config_list_name in ["additional_outputs", "mandatory_outputs"]:
+        if not isinstance(config[config_list_name], list):
+            raise TypeError(
+                f"""
+                In config file {config_list_name} must be a list, please use:\n
+                ["all"] to get all outputs\n
+                [] to get only mandatory outputs (defined in dev config) (\n
+                or a list with the outputs, e.g. ["output_1","output_2"]
+                """
+            )
 
     if config["additional_outputs"] == ["all"]:
         # Dont have to worry about mandatory outputs here, all functions from mapper
@@ -25,8 +55,8 @@ def get_additional_outputs(
     else:
         # If "all" is not specified, use the provided list combining with
         # mandatory outputs
-        functions_to_run = list(
-            set(config["additional_outputs"]) | set(config["mandatory_outputs"])
+        functions_to_run = sorted(
+            list(set(config["additional_outputs"]) | set(config["mandatory_outputs"]))
         )
 
     for function in functions_to_run:
