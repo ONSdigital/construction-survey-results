4d4f1c2ad28f57d43d1931e03b582634043b7f7c
daviel9 <Luke.Davies@ons.gov.uk>
2025-07-03 09:32:53 +0100

    Re-do reverted changes

--- cons_results/staging/stage_dataframe.py
+++ cons_results/staging/stage_dataframe.py
@@ -32,44 +32,46 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
         Combined dataframe containing response and contributor data
     """
 
-    period = config["period"]
-    reference = config["reference"]
-    snapshot_file_path = config["snapshot_file_path"]
+    staging_config = config.copy()
+    period = staging_config["period"]
+    reference = staging_config["reference"]
+    snapshot_file_path = staging_config["snapshot_file_path"]
 
     contributors, responses = get_dfs_from_spp(
         snapshot_file_path,
-        config["platform"],
-        config["bucket"],
+        staging_config["platform"],
+        staging_config["bucket"],
     )
 
     # Filter columns and set data types
-    contributors = contributors[config["contributors_keep_cols"]]
+    contributors = contributors[staging_config["contributors_keep_cols"]]
     contributors = enforce_datatypes(
-        contributors, keep_columns=config["contributors_keep_cols"], **config
+        contributors,
+        keep_columns=staging_config["contributors_keep_cols"],
+        **staging_config,
     )
 
     # Drop imputation marker for contributors as it is only neccessary for responses
-    contributors = append_back_data(contributors, config).drop(
-        columns=[config["imputation_marker_col"]]
+    contributors = append_back_data(contributors, staging_config).drop(
+        columns=[staging_config["imputation_marker_col"]]
     )
 
-    responses = responses[config["responses_keep_cols"]]
+    responses = responses[staging_config["responses_keep_cols"]]
     responses = enforce_datatypes(
-        responses, keep_columns=config["responses_keep_cols"], **config
+        responses, keep_columns=staging_config["responses_keep_cols"], **staging_config
     )
 
-    responses = append_back_data(responses, config)
+    responses = append_back_data(responses, staging_config)
 
     # Add an extra month to the revison window to include the back data
-    back_data_config = config.copy()
-    back_data_config["revision_window"] = config["revision_window"] + 1
+    staging_config["revision_window"] = config["revision_window"] + 1
 
-    finalsel = read_and_combine_colon_sep_files(back_data_config)
+    finalsel = read_and_combine_colon_sep_files(staging_config)
 
     # keep columns is applied in data reading from source, enforcing dtypes
     # in all columns of finalsel
     finalsel = enforce_datatypes(
-        finalsel, keep_columns=list(finalsel), **back_data_config
+        finalsel, keep_columns=list(finalsel), **staging_config
     )
 
     # Filter contributors files here to temp fix this overlap
@@ -85,10 +87,10 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
     df = create_missing_questions(
         contributors=contributors,
         responses=responses,
-        all_questions=back_data_config["all_questions"],
-        reference=back_data_config["reference"],
-        period=back_data_config["period"],
-        question_col=back_data_config["question_no"],
+        all_questions=staging_config["all_questions"],
+        reference=staging_config["reference"],
+        period=staging_config["period"],
+        question_col=staging_config["question_no"],
     )
 
     df = pd.merge(left=df, right=contributors, on=[period, reference], how="left")
@@ -97,60 +99,58 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
 
     df = filter_out_questions(
         df=df,
-        column=back_data_config["question_no"],
-        questions_to_filter=back_data_config["filter_out_questions"],
-        save_full_path=back_data_config["output_path"]
+        column=staging_config["question_no"],
+        questions_to_filter=staging_config["filter_out_questions"],
+        save_full_path=staging_config["output_path"]
         + snapshot_name
         + "_filter_out_questions.csv",
-        **back_data_config,
+        **staging_config,
     )
 
     df = run_live_or_frozen(
         df,
-        back_data_config["target"],
-        status=back_data_config["status"],
-        state=back_data_config["state"],
+        staging_config["target"],
+        status=staging_config["status"],
+        state=staging_config["state"],
         error_values=[201],
     )
 
-    df[back_data_config["auxiliary_converted"]] = df[
-        back_data_config["auxiliary"]
-    ].copy()
-    df = convert_annual_thousands(df, back_data_config["auxiliary_converted"])
+    df[staging_config["auxiliary_converted"]] = df[staging_config["auxiliary"]].copy()
+    df = convert_annual_thousands(df, staging_config["auxiliary_converted"])
 
     df = derive_imputation_class(
         df,
-        back_data_config["bands"],
-        back_data_config["cell_number"],
-        back_data_config["imputation_class"],
+        staging_config["bands"],
+        staging_config["cell_number"],
+        staging_config["imputation_class"],
     )
 
-    if back_data_config["manual_constructions_path"]:
+    if staging_config["manual_constructions_path"]:
         manual_constructions = read_csv_wrapper(
-            back_data_config["manual_constructions_path"],
-            back_data_config["platform"],
-            back_data_config["bucket"],
+            staging_config["manual_constructions_path"],
+            staging_config["platform"],
+            staging_config["bucket"],
         )
     else:
         manual_constructions = None
 
-    if back_data_config["filter"]:
+    if staging_config["filter"]:
         filter_df = read_csv_wrapper(
-            back_data_config["filter"],
-            back_data_config["platform"],
-            back_data_config["bucket"],
+            staging_config["filter"],
+            staging_config["platform"],
+            staging_config["bucket"],
         )
-        filter_df = enforce_datatypes(filter_df, list(filter_df), **back_data_config)
+        filter_df = enforce_datatypes(filter_df, list(filter_df), **staging_config)
 
     else:
         filter_df = None
 
     df = flag_290_case(
         df,
-        back_data_config["period"],
-        back_data_config["reference"],
-        back_data_config["question_no"],
-        back_data_config["target"],
+        staging_config["period"],
+        staging_config["reference"],
+        staging_config["question_no"],
+        staging_config["target"],
     )
 
     print("Staging Completed")
