1dbc710f5dcb951774a5f5da90e70ed94076050d
hubbal <luke.hubbard@ons.gov.uk>
2025-08-07 09:07:46 +0100

    Skeleton standard errors function and test data

---
+++ cons_results/outputs/standard_errors.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+def create_standard_errors(additional_outputs_df, **config):
+    """
+    
+
+    Parameters
+    ----------
+    additional_outputs_df : TYPE
+        DESCRIPTION.
+    **config : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    #TODO: Std errors created from unweighted or weighted vals?
+    
+    
