2e531a3f96842e4588e40d6678fb0bda52db1cca
Anton Zogkolli <anton.zogkolli@ons.gov.uk>
2025-08-07 12:16:52 +0100

    Add more scenarios in total as zero

--- tests/staging/test_total_as_zero.py
+++ tests/staging/test_total_as_zero.py
@@ -8,10 +8,10 @@ from cons_results.staging.total_as_zero import flag_total_only_and_zero
 @pytest.fixture()
 def input_df():
     data = {
-        "reference": [1, 2, 2, 2, 2, 3],
-        "period": [202201, 202201, 202201, 202201, 202201, 202201],
-        "qcodes": [290, 290, 2, 3, 4, 4],
-        "values": [0, 150, 75, 75, 0, 0],
+        "reference": [1, 2, 2, 2, 2, 3, 5, 5, 5, 6],
+        "period": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        "qcodes": [290, 290, 2, 3, 4, 4, 290, 2, 3, 290],
+        "values": [0, 150, 75, 75, 0, 0, 0, 99, 99, 99],
     }
     df = pd.DataFrame(data)
     return df
@@ -20,11 +20,22 @@ def input_df():
 @pytest.fixture()
 def expected_output_df():
     data = {
-        "reference": [1, 2, 2, 2, 2, 3],
-        "period": [202201, 202201, 202201, 202201, 202201, 202201],
-        "qcodes": [290, 290, 2, 3, 4, 4],
-        "values": [0, 150, 75, 75, 0, 0],
-        "is_total_only_and_zero": [True, False, False, False, False, False],
+        "reference": [1, 2, 2, 2, 2, 3, 5, 5, 5, 6],
+        "period": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        "qcodes": [290, 290, 2, 3, 4, 4, 290, 2, 3, 290],
+        "values": [0, 150, 75, 75, 0, 0, 0, 99, 99, 99],
+        "is_total_only_and_zero": [
+            True,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+        ],
     }
     df = pd.DataFrame(data)
 
