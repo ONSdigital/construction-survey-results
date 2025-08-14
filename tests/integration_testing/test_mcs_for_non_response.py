d3f126e6d2d2f0843adc474c753b0fa8a4ae733f
Nathan Kelly <nathan.kelly@ons.gov.uk>
2025-08-14 17:29:26 +0100

    Update unit tests to cover manual constructions edge case

---
+++ tests/integration_testing/test_mcs_for_non_response.py
@@ -0,0 +1,100 @@
+import json
+from pathlib import Path
+
+import pandas as pd
+import pytest
+
+from cons_results.imputation.impute import impute
+from cons_results.staging.stage_dataframe import stage_dataframe
+
+
+@pytest.fixture(scope="class")
+def filepath():
+    return Path("tests/data/integration_testing")
+
+
+@pytest.fixture(scope="class")
+def test_config(filepath):
+    return {
+        "platform": "network",
+        "bucket": "",
+        "calibration_group_map_path": "",
+        "classification_values_path": None,
+        "idbr_folder_path": filepath,
+        "l_values_path": None,
+        "manual_outlier_path": "",
+        "snapshot_file_path": "",
+        "manual_constructions_path": str(filepath / "manual_constructions.csv"),
+        "filter": None,
+        "output_path": str(filepath),
+        "population_prefix": None,
+        "sample_prefix": "finalsel228",
+        "back_data_qv_path": filepath / "test_qv_009_202412.csv",
+        "back_data_cp_path": filepath / "test_cp_009_202412.csv",
+        "back_data_finalsel_path": str(filepath / "finalsel228_dummy_202412"),
+        "sic_domain_mapping_path": "",
+        "current_period": 202503,
+        "revision_window": 3,
+        "state": "frozen",
+        "optional_outputs": [""],
+        "components_questions": [1, 2, 3, 4, 5, 6],
+    }
+
+
+def load_config(config_file_path):
+    with open(config_file_path, "r") as f:
+        return json.load(f)
+
+
+def load_config_temp():
+    config_dev_path = "cons_results/configs/config_dev.json"
+    config = load_config(config_dev_path)
+    return config
+
+
+@pytest.mark.parametrize(
+    "snapshot_file,expected_output_file",
+    [
+        ("mc_for_non_response.json", "mc_for_non_response.csv"),
+        ("mc_for_non_response_2.json", "mc_for_non_response_2.csv"),
+    ],
+)
+def test_mc_for_non_response(
+    test_config, filepath, snapshot_file, expected_output_file
+):
+    """Run Staging and Imputation based on test_config"""
+    config = load_config_temp()
+    config.update(test_config)
+    config["snapshot_file_path"] = str(filepath / snapshot_file)
+
+    df, manual_constructions, filter_df = stage_dataframe(config)
+
+    df = impute(df, config, manual_constructions, filter_df)
+
+    cols_output = [
+        "reference",
+        "period",
+        "questioncode",
+        "status",
+        "imputation_flags_adjustedresponse",
+    ]
+
+    # Load expected output DataFrame
+    expected_output_path = filepath / expected_output_file
+    expected_df = pd.read_csv(expected_output_path)
+
+    # Sort both DataFrames for consistent comparison
+    df_sorted = (
+        df[cols_output]
+        .sort_values(by=["period", "questioncode"])
+        .reset_index(drop=True)
+    )
+
+    cols_expected = df_sorted.columns.to_list()
+    expected_sorted = (
+        expected_df[cols_expected]
+        .sort_values(by=["period", "questioncode"])
+        .reset_index(drop=True)
+    )
+
+    pd.testing.assert_frame_equal(df_sorted, expected_sorted)
