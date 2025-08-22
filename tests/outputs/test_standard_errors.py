d702e418356ab6550ac0aa5960ecd461e1623938
hubbal <luke.hubbard@ons.gov.uk>
2025-08-22 11:08:44 +0100

    Fixing sample config in unit test

--- tests/outputs/test_standard_errors.py
+++ tests/outputs/test_standard_errors.py
@@ -13,21 +13,17 @@ def filepath():
 def sample_config(filepath):
     return {
         "period": "period",
-        "region": "region",
+        "sic": "frosic2007",
         "question_no": "questioncode",
         "target": "adjustedresponse",
-        "produce_quarterly_extracts": True,
-        "region_mapping_path": filepath + "region_mapping.csv",
-        "output_path": filepath,
+        "cell_number": "cell_no",
     }
 
 
 def test_quarterly_extracts(filepath, sample_config):
 
     input_df = pd.read_csv(filepath + "input.csv")
-
     expected_output = pd.read_csv(filepath + "output.csv")
-
-    actual_output = create_standard_errors(input_df)
+    actual_output = create_standard_errors(input_df, **sample_config)
 
     pd.testing.assert_frame_equal(actual_output, expected_output)
