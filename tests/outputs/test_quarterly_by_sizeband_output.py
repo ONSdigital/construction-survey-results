02de0db721bb9734082c9a5edceb57ed00ce6d23
Nathan Kelly <nathan.kelly@ons.gov.uk>
2025-09-02 14:27:58 +0100

    Update components questions in test config

--- tests/outputs/test_quarterly_by_sizeband_output.py
+++ tests/outputs/test_quarterly_by_sizeband_output.py
@@ -35,7 +35,7 @@ def config():
         "question_no": "questioncode",
         "target": "adjustedresponse",
         "cell_number": "cell_no",
-        "components_questions": [1001, 1002],
+        "components_questions": [40, 49],
     }
 
 
