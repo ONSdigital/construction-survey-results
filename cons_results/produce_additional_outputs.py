d1692af29d0efec3cc426bd570e2e54f1ce1bbc4
Anton Zogkolli <anton.zogkolli@ons.gov.uk>
2025-09-29 14:46:20 +0100

    Add cons additional outputs wrapper and test

---
+++ cons_results/produce_additional_outputs.py
@@ -0,0 +1,24 @@
+
+from mbs_results.utilities.inputs import load_config, read_csv_wrapper
+
+from cons_results.outputs.produce_additional_outputs import produce_additional_outputs
+
+
+def produce_additional_outputs_wrapper(config_user_dict=None):
+    """Produces any additional outputs based on MBS methods output"""
+
+    config = load_config("config_outputs.json", config_user_dict)
+
+    df = read_csv_wrapper(
+        filepath=config["cons_output_path"],
+        import_platform=config["platform"],
+        bucket_name=config["bucket"],
+    )
+
+    produce_additional_outputs(
+        additional_outputs_df=df, qa_outputs=False, optional_outputs=True, config=config
+    )
+
+
+if __name__ == "__main__":
+    produce_additional_outputs_wrapper()
---
+++ tests/test_produce_additional_outputs.py
@@ -0,0 +1,38 @@
+
+
+from cons_results.produce_additional_outputs import produce_additional_outputs_wrapper
+
+
+from pathlib import Path
+
+import pytest
+
+@pytest.fixture(scope="class")
+def filepath():
+    return Path("tests/data/test_main/input/")
+
+@pytest.fixture(scope="class")
+def test_config(filepath):
+
+    return  {
+            "platform": "network",
+            "bucket": "",
+            "idbr_folder_path": filepath,
+            "snapshot_file_path": str(filepath / "dummy_snapshot.json"),
+            "cons_output_path": "tests/data/test_main/output/expected_from_cons_main.csv",
+            "cdid_data_path": "tests/data/outputs/csdb_output/cdid_mapping.csv",
+            "output_path": "tests/data/test_main/output/",
+            "current_period": 202206,
+            "revision_window": 6,
+            "region_mapping_path": filepath / "region_code_name_mapping.csv",
+            "r_and_m_quarter": None,
+            "sizeband_quarter": ["2023Q2"],
+            "imputation_contribution_periods": [202302]
+    }
+      
+def test_produce_additional_outputs_wrapper(test_config):
+    """Triggers a produce_additional_outputs_wrapper run, the output
+    of mbs main is the input of this"""
+
+
+    produce_additional_outputs_wrapper(config_user_dict=test_config)
