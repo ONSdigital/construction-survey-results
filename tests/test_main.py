ee0e6fe8c23e4656297f47d1dec64793cc2040fc
Nathan Kelly <nathan.kelly@ons.gov.uk>
2025-10-13 16:55:13 +0100

    Update test data to pass tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -53,7 +53,9 @@ class TestMain:
         actual = pd.read_csv(patern[0])
         expected = pd.read_csv(out_path + "expected_from_cons_main.csv")
 
-        assert_frame_equal(actual, expected)
+        actual.to_csv("actual_output_test_main.csv", index=False)
+
+        assert_frame_equal(actual, expected, check_like=False)
 
     def test_run_pipeline_live(self, test_config):
         """Run main pipeline based on test_config"""
