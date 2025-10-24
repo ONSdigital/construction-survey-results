52f814211b3371897d3b553e5e7d80968297861c
Anton Zogkolli <anton.zogkolli@ons.gov.uk>
2025-10-24 11:17:40 +0100

    Add run id to validation extract

--- cons_results/imputation/impute.py
+++ cons_results/imputation/impute.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from mbs_results.imputation.ratio_of_means import ratio_of_means
 from mbs_results.staging.data_cleaning import convert_annual_thousands
+from mbs_results.utilities.utils import get_versioned_filename
 
 from cons_results.imputation.post_imputation import (
     create_q290,
@@ -123,6 +124,8 @@ def impute(
         config["target"],
     )
 
+    validate_q290_file_name = get_versioned_filename("validate_q290_output", config)
+
     validate_q290(
         df=df,
         question_no=config["question_no"],
@@ -130,7 +133,7 @@ def impute(
         reference=config["reference"],
         adjustedresponse=config["target"],
         output_path=config["output_path"],
-        output_file_name="validate_q290_output.csv",
+        output_file_name=validate_q290_file_name,
         import_platform=config["platform"],
         bucket_name=config["bucket"],
     )
