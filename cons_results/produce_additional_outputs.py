ab963f62ab26e2b5d94ca15c09884532e867c25a
jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-12-16 16:22:48 +0000

    Adding logger into additional outputs wrapper

--- cons_results/produce_additional_outputs.py
+++ cons_results/produce_additional_outputs.py
@@ -1,4 +1,5 @@
 from mbs_results.utilities.inputs import load_config, read_csv_wrapper
+from mbs_results.utilities.setup_logger import setup_logger, upload_logger_file_to_s3
 from mbs_results.utilities.utils import get_or_read_run_id, get_versioned_filename
 
 from cons_results.outputs.produce_additional_outputs import produce_additional_outputs
@@ -9,6 +10,11 @@ def produce_additional_outputs_wrapper(config_user_dict=None):
 
     config = load_config("config_outputs.json", config_user_dict)
     config["run_id"] = get_or_read_run_id(config)
+
+    # Initialise the logger at the start of the pipeline
+    logger_file_path = f"cons_additional_outputs_{config['run_id']}.log"
+    logger = setup_logger(logger_file_path=logger_file_path)
+    logger.info(f"Cons Additional Outputs Started: Log file: {logger_file_path}")
 
     output_file_name = get_versioned_filename(
         config["cons_output_prefix"],
@@ -32,6 +38,8 @@ def produce_additional_outputs_wrapper(config_user_dict=None):
     produce_additional_outputs(
         additional_outputs_df=df, qa_outputs=False, optional_outputs=True, config=config
     )
+
+    upload_logger_file_to_s3(config, logger_file_path)
 
 
 if __name__ == "__main__":
